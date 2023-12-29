faa2197cc29b47a6aee2b31e953373c248c0cbc0
Chris Karpyszyn <ckarpyszyn@uncommonpurpose.com>
2023-12-29 10:46:09 -0500

    capture keyboard input

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public TextMeshPro tmp;
+    
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        //sneaky exit
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+
+        if (Input.anyKeyDown && !isMouseButtonClick())
+        {
+            Debug.Log(Input.inputString);
+        }
+
+
+    }
+
+    private bool isMouseButtonClick()
+    {
+        return (Input.GetMouseButton(0) || Input.GetMouseButton(1) || Input.GetMouseButton(2));
     }
 }
