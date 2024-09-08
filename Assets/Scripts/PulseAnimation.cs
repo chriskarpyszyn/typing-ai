bae4021a3a5ca2ddd3f0b6ab0a72984d859612c0
Chris Karpyszyn <chris.karpyszyn@gmail.com>
2024-09-08 09:50:09 -0400

    fix title animation, word animations broken

---
+++ Assets/Scripts/PulseAnimation.cs
@@ -0,0 +1,19 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseAnimation : MonoBehaviour
+{
+    [SerializeField] private float scaleMin;
+    [SerializeField] private float scaleMax;
+    [SerializeField] private float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sequence seq = DOTween.Sequence().SetLoops(-1);
+        seq.Append(transform.DOScale(scaleMax, duration).SetEase(Ease.Linear))
+            .Append(transform.DOScale(scaleMin, duration).SetEase(Ease.Linear));
+    }
+}
