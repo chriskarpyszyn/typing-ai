7ed43695ecdfe6d0c1e9ef7cf1bce6f70adb4c12
Chris Karpyszyn <chris.karpyszyn@gmail.com>
2024-09-16 10:01:58 -0400

    build new scene, initial moving prototype

---
+++ Assets/Scripts/AstroidMovement.cs
@@ -0,0 +1,23 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidMovement : MonoBehaviour
+{
+
+    [SerializeField] private float asteroidSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.DOAnchorPos(new Vector3(0, 0, -1), asteroidSpeed).SetEase(Ease.Linear);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
