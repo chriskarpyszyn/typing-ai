0c14e62f19b70c6913d5e11c1d3244f9ecf52cd0
Chris Karpyszyn <chris.karpyszyn@gmail.com>
2024-09-30 10:21:27 -0400

    words are working need to destroy

--- Assets/Scripts/Handlers/LevelManager.cs
+++ Assets/Scripts/Handlers/LevelManager.cs
@@ -30,6 +30,9 @@ public class LevelManager : MonoBehaviour
         if (wordsCompletedInCurrentLevel >= wordsPerLevel)
         {
             AdvanceLevel();
+        } else
+        {
+            //next word...
         }
     }
 
